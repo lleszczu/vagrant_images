cc82d3446bcf329b6b075ed4a8078990db8480cc
Lukasz Leszczuk <lukasz.leszczuk@intel.com>
2016-12-19 18:01:33 +0100

    final fixes for coreos stable release

--- coreos/install.sh
+++ coreos/install.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-env
-IP=${HTTP_IP}
-PORT=${HTTP_PORT}
 
-#wget http://${IP}:${PORT}/cloud-config.yml
-#coreos-install -d /dev/vda -c cloud-config.yml
+sudo coreos-install -d /dev/vda -c /tmp/cloud-config.yml
